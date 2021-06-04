348bf9a8fa48776acfcdfc1be41ec04325465cad
Mike Calvanese <mfcalvanese@gmail.com>
2021-06-04 15:52:25 -0400

    make splitCallData work with or without prefixed `0x`

--- src/splitCallData.js
+++ src/splitCallData.js
@@ -1,12 +1,13 @@
 function splitCallData (callData, numSignedParams) {
+  let parsedCallData = callData.indexOf('0x') == 0 ? callData.slice(2) : callData
   // signed data is the prefix + fnSig + signedParams
   const bytes32SlotLen = 64
   const fnSigLen = 8
   const signedDataLen = fnSigLen + (numSignedParams * bytes32SlotLen)
-  const signedData = `0x${callData.slice(0, signedDataLen)}`
+  const signedData = `0x${parsedCallData.slice(0, signedDataLen)}`
 
   // unsigned data is the rest
-  const unsignedData = `0x${callData.slice(signedDataLen)}`
+  const unsignedData = `0x${parsedCallData.slice(signedDataLen)}`
   return { signedData, unsignedData }
 }
 
