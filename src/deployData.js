8d1237c072abcf2232da67eb840470d638c85901
Mike Calvanese <mfcalvanese@gmail.com>
2021-08-17 16:43:15 -0400

    remove chainId from proxy deploy params

--- src/deployData.js
+++ src/deployData.js
@@ -1,8 +1,8 @@
 const web3Utils = require('web3-utils')
 const computeAccountBytecode = require('./computeAccountBytecode')
 
-const deployData = (deployerAddress, proxyBytecode, implementationAddress, ownerAddress, chainId, accountDeploymentSalt) => {
-  const initCode = computeAccountBytecode(proxyBytecode, implementationAddress, ownerAddress, chainId)
+const deployData = (deployerAddress, proxyBytecode, implementationAddress, ownerAddress, accountDeploymentSalt) => {
+  const initCode = computeAccountBytecode(proxyBytecode, implementationAddress, ownerAddress)
   const codeHash = web3Utils.soliditySha3({ t: 'bytes', v: initCode })
   const addressAsBytes32 = web3Utils.soliditySha3(
     { t: 'uint8', v: 255 }, // 0xff
